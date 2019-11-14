2a8724360445af3292c2dd96f6fe52ae3cf32631
Poornima Nookala <pnookala@anl.gov>
2019-11-14 17:49:13 +0000

    fixed nested_abt for mm

--- examples/nested_abt.c
+++ examples/nested_abt.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <abt.h>
 #include <unistd.h>
+#include <math.h>
 #include <sys/time.h>
+#include <immintrin.h>
 
+#define SIZE 1024
+
+typedef double real_t;
 static int main_num_es = 4;
 static int inner_num_es = 4;
+
+real_t * matA; 
+real_t * matB; 
+real_t * matC;
+real_t * result;
 
 typedef struct {
 	int start_i;
@@ -14,18 +24,32 @@ typedef struct {
 
 typedef void (*inner_f)(void*);
 
-void work_f(void* args) {
-	//printf("Hello World\n");
-	for(int i = 0; i < 100000000; i++) {
+void work_f(void* data) {
+	/*for(int i = 0; i < 100000000; i++) {
         asm volatile("");
+    }*/
+    threadData* args = (threadData*)data;
+    for (int64_t j = args->start_i; j < args->end_i; j++)
+    {
+        for (int64_t i = 0; i < SIZE; i++)
+        {
+            //__m128d c = _mm_setzero_pd();
+            for (int64_t k = 0; k < SIZE; k += 1)
+                //c = _mm_add_pd(c, _mm_mul_pd(
+                //        _mm_load_pd(&matA[k + j * SIZE]),
+                //        _mm_load_pd(&matB[i + k * SIZE])));
+                matC[i + j * SIZE] += matA[k + j * SIZE] * matB[i + k * SIZE];
+            //c = _mm_add_pd(c, c);
+            //_mm_store_sd(&matC[i + j * SIZE], c);
+        }
     }
-    /*threadData* data = (threadData*)args;
-  
-    for (int i = data->start_i; i < data->end_i; i++)  
+    /*for (int i = data->start_i; i < data->end_i; i++) { 
          for (int j = 0; j < SIZE; j++)  
                 for (int k = 0; k < SIZE; k++)  
     	          matC[i][j] += matA[i][k] * matB[k][j];
-*/
+         
+    }*/
+
 }
 
 typedef struct {
@@ -80,18 +104,19 @@ void abt_for(int num_threads, int loop_count, inner_f inner_func, int level) {
 
   /* ULT creation */
   threads = (abt_thread_data_t *)malloc(sizeof(abt_thread_data_t) * loop_count);
-  int num_ult_per_pool = loop_count/num_threads;
+  int each = SIZE/loop_count;
+  int leftover = SIZE % loop_count;
   //printf("Each thread processes %d size matrix\n", each);
-  for (i = 0; i < num_threads; i++) {
-     for(int j = 0; j < num_ult_per_pool; j++) {
-  //      threads[i].inner_func = inner_func;
-  //      threads[i].arg = (threadData*)malloc(sizeof(threadData));
-  //      threads[i].arg->start_i = i * each;
-  //      threads[i].arg->end_i = threads[i].arg->start_i + each - 1;
+  for (i = 0; i < loop_count; i++) {
+        threads[i].inner_func = inner_func;
+        threads[i].arg = (threadData*)malloc(sizeof(threadData));
+        threads[i].arg->start_i = i * each;
+        threads[i].arg->end_i = threads[i].arg->start_i + each;
+        if( i == loop_count - 1)
+            threads[i].arg->end_i = threads[i].arg->end_i + leftover;
         //printf("start_i %d, end_i %d\n", threads[i].arg->start_i, threads[i].arg->end_i);
-        ABT_thread_create(pools[i], inner_func, NULL,
-                      ABT_THREAD_ATTR_NULL, &threads[i*num_ult_per_pool+j].thread);
-    }
+        ABT_thread_create(pools[i], inner_func_wrapper, &threads[i],
+                      ABT_THREAD_ATTR_NULL, &threads[i].thread);
   }
 
 //  printf("threads created...joining threads!\n");
@@ -125,8 +150,16 @@ void inner2_par(int i) {
 }
 
 void inner_par(void* data) {
- // printf("inner abt_for\n");
-  abt_for(inner_num_es, inner_num_es*144, work_f, 1);
+  //printf("inner abt_for\n");
+  abt_for(inner_num_es, inner_num_es, work_f, 1);
+}
+
+void matmul_serial()
+{
+    for (int64_t j = 0; j < SIZE; j++)
+        for (int64_t i = 0; i < SIZE; i++)
+            for (int64_t k = 0; k < SIZE; k++)
+                result[i + j * SIZE] += matA[k + j * SIZE] * matB[i + k * SIZE];
 }
 
 int main (int argc, char** argv) {
@@ -140,12 +173,40 @@ int main (int argc, char** argv) {
     inner_num_es = atoi(argv[2]);
     summary_file = argv[3];
   }
+
+    matA = (real_t *)malloc(sizeof(real_t) * SIZE * SIZE);
+    matB = (real_t *)malloc(sizeof(real_t) * SIZE * SIZE);
+    matC = (real_t *)malloc(sizeof(real_t) * SIZE * SIZE);
+    result = (real_t *)malloc(sizeof(real_t) * SIZE * SIZE);
+    for (int64_t j = 0; j < SIZE; j++) {
+        for (int64_t i = 0; i < SIZE; i++) {
+            // Any random values
+            matA[i + j* SIZE] = sin((double)(i + j));
+            matB[i + j* SIZE] = cos((double)(i + 2 * j));
+            matC[i + j* SIZE] = 0.0;
+            result[i + j* SIZE] = 0.0;
+        }
+    }
+
   struct timeval start;
   struct timeval stop;
   gettimeofday(&start, NULL);
   abt_for(main_num_es, main_num_es, inner_par, 0);
   gettimeofday(&stop, NULL);
 
+  /* Verify */
+  matmul_serial();
+ 
+  for (int64_t j = 0; j < SIZE; j++) {
+            for (int64_t i = 0; i < SIZE; i++) {
+                if ((matC[i + j * SIZE] - result[i + j * SIZE]) > (real_t)0.0001) {
+                    printf("Different: c[%d,%d] (=%f) != %f\n", (int)i, (int)j,
+                           matC[i + j * SIZE], result[i + j * SIZE]);
+                    break;
+                }
+            }
+        }
+ 
   float elapsed_time = (float)(stop.tv_sec - start.tv_sec + 
                 (stop.tv_usec - start.tv_usec)/(float)1000000);  
     
